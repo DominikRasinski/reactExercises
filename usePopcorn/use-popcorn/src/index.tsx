c0fb72db6cc9e451db9e97a139830e027db62bae
Dominik Rasiński <dox.gmr@gmail.com>
2024-07-04 19:15:45 +0200

    add star component for rating

--- usePopcorn/use-popcorn/src/index.tsx
+++ usePopcorn/use-popcorn/src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Stars } from './components/stars/Stars';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Stars maxRating={5} />
+    {/* <App /> */}
   </React.StrictMode>
 );
